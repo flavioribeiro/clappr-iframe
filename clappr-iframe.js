d8cd5af07cf3c6f8f1545ed0a17da2f9129bd6f2
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-11-27 11:40:29 -0200

    add basic clapprLoader function

---
+++ clappr-iframe.js
@@ -0,0 +1,29 @@
+var createClapprIframe = function(params, queryString) {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("frameborder", 0);
+    iframe.setAttribute("allowfullscreen", true);
+    iframe.setAttribute("scrolling", "no");
+    iframe.setAttribute('width', params.width || 640);
+    iframe.setAttribute('height', params.height || 360);
+    iframe.setAttribute('src', "http://localhost:8000/player.html" + queryString);
+    iframe.attachTo = function(el) {
+      el.appendChild(this);
+    };
+    return iframe;
+};
+
+var buildQueryString = function(params) {
+  var result = "";
+  for (var param in params) {
+    result += !!result? "&" : "?";
+    result += encodeURIComponent(param) + "=" + encodeURIComponent(params[param]);
+  }
+  return result;
+};
+
+clapprLoader = function(params) {
+  var queryString = buildQueryString(params);
+  return createClapprIframe(params, queryString);
+};
+
+
